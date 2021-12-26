d68c21ac55e5c3e56321e3e7da35d1f0b8c57e8d
Daniel Volk <mail@volkarts.com>
2021-12-26 22:06:25 +0100

    use vim.opt

--- lua/local/basics.lua
+++ lua/local/basics.lua
@@ -1,32 +1,30 @@
-vim.o.encoding = 'utf-8'
-vim.o.clipboard = 'unnamedplus'
-vim.o.cursorline = true
-vim.o.hidden = true -- Allow modified buffers in background
-vim.o.inccommand = 'split' -- Show replacements before applieng
--- vim.o.mouse=a
-vim.o.number = true
-vim.o.signcolumn = 'yes'
-vim.o.relativenumber = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.title = true
-vim.o.ttimeoutlen = 0 -- Command timeout
-vim.o.wildmenu = true -- Auto complete menu
-vim.o.wrap = false
-vim.o.hlsearch = true
-vim.o.incsearch = true
-vim.o.ignorecase = true
-vim.o.scrolloff = 3
-vim.o.ruler = true
-vim.o.undofile = true; vim.bo.undofile = true
-vim.o.path = vim.o.path .. '.,**'
+vim.opt.encoding = 'utf-8'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
+vim.opt.hidden = true -- Allow modified buffers in background
+vim.opt.inccommand = 'split' -- Show replacements before applieng
+-- vim.opt.mouse=a
+vim.opt.number = true
+vim.opt.signcolumn = 'yes'
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.title = true
+vim.opt.ttimeoutlen = 0 -- Command timeout
+vim.opt.wildmenu = true -- Auto complete menu
+vim.opt.wrap = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 3
+vim.opt.ruler = true
+vim.opt.undofile = true; vim.bo.undofile = true
+vim.opt.path:append('.,**')
 
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
--- vim.o.whichwrap+=<,>,[,]
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+-- vim.opt.whichwrap+=<,>,[,]
 
-vim.o.list = true
--- vim.o.listchars = {tab = '→\\ ', nbsp = '␣', trail = '•', extends = '⟩', precedes = '⟨'}
---  vim.o.listchars = { tab = '→ ' }
--- vim.o.listchars = { trail = '1' }
+vim.opt.list = true
+vim.opt.listchars = {tab = '→\\ ', nbsp = '␣', trail = '•', extends = '⟩', precedes = '⟨'}
