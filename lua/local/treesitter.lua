2483ed0e6c42a846f728c58b6cf78e3c37719f99
Daniel Volk <mail@volkarts.com>
2022-01-02 22:38:52 +0100

    Enable treesitter

---
+++ lua/local/treesitter.lua
@@ -0,0 +1,20 @@
+local ok, config = pcall(require, 'nvim-treesitter.configs')
+if not ok then
+    print "Plugin treesitter not found"
+    return
+end
+
+config.setup({
+    ensure_installed = "maintained",
+    sync_install = false,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlight = true,
+    },
+    indent = {
+        enable = true,
+        disable = {},
+    }
+})
