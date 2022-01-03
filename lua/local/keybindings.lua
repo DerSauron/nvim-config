43a9b24f72b8a914fa7a48656dae7b342decf17d
Daniel Volk <mail@volkarts.com>
2022-01-03 15:35:10 +0100

    Cleanup keybindings file

--- lua/local/keybindings.lua
+++ lua/local/keybindings.lua
@@ -8,10 +8,10 @@ vmap("<C-A-y>", "\"+y")
 vmap("<C-A-p>", "\"+p")
 
 -- Move text up and down
-nmap("<A-j>", "<cmd>m .+1<CR>==")
-nmap("<A-k>", "<cmd>m .-2<CR>==")
-vmap("<A-j>", "<cmd>m .+1<CR>==gv")
-vmap("<A-k>", "<cmd>m .-2<CR>==gv")
+nmap("<A-j>", "<Cmd>m .+1<CR>==")
+nmap("<A-k>", "<Cmd>m .-2<CR>==")
+vmap("<A-j>", "<Cmd>m .+1<CR>==gv")
+vmap("<A-k>", "<Cmd>m .-2<CR>==gv")
 
 -- Visual indent fix
 vmap("<", "<gv")
@@ -22,14 +22,14 @@ vmap("=", "=gv")
 nmap("<Leader>/", "<Cmd>lua require(\"Comment\").toggle()<CR>")
 vmap("<Leader>/", "<Cmd>lua require(\"Comment.api\").gc(vim.fn.visualmode())")
 
--- chad tree
-nmap("<leader>e", "<cmd>NvimTreeToggle<CR>")
+-- nvim tree
+nmap("<Leader>e", "<Cmd>NvimTreeToggle<CR>")
 
 -- telescope
-nmap("<leader>ff", "<cmd>Telescope find_files<cr>")
-nmap("<leader>fg", "<cmd>Telescope live_grep<cr>")
-nmap("<leader>fb", "<cmd>Telescope buffers<cr>")
-nmap("<leader>fh", "<cmd>Telescope help_tags<cr>")
+nmap("<Leader>ff", "<Cmd>Telescope find_files<CR>")
+nmap("<Leader>fg", "<Cmd>Telescope live_grep<CR>")
+nmap("<Leader>fb", "<Cmd>Telescope buffers<CR>")
+nmap("<Leader>fh", "<Cmd>Telescope help_tags<CR>")
 
 -- terminal
 tmap("<ESC>", "<C-\\><C-n>")
