bee90666e1bd606a52d93ee6a86bb47a391e2bc2
Daniel Volk <mail@volkarts.com>
2022-01-07 16:42:23 +0100

    Fix comment key bindings

--- lua/local/keybindings.lua
+++ lua/local/keybindings.lua
@@ -24,8 +24,8 @@ nmap("<Leader>dw", "\"_dw")
 nmap("<Leader>dd", "\"_dd")
 
 -- commenting
-nmap("<Leader>/", "<Cmd>lua require(\"Comment\").toggle()<CR>")
-vmap("<Leader>/", "<Cmd>lua require(\"Comment.api\").gc(vim.fn.visualmode())")
+nmap("<C-_>", "<Cmd>lua require(\"Comment.api\").call(\"toggle_current_linewise_op\")<CR>g@$")
+xmap("<C-_>", "<Esc><Cmd>lua require(\"Comment.api\").toggle_linewise_op(vim.fn.visualmode())<CR>")
 
 -- nvim tree
 nmap("<Leader>e", "<Cmd>NvimTreeToggle<CR>")
